dafef7f12cf93ab3f06aaf55885082d2489adb88
Benedikt Hilmes <benedikt.hilmes@rwth-aachen.de>
2023-04-26 16:55:37 +0200

    simplifications + test examples

--- i6_models/assemblies/util.py
+++ i6_models/assemblies/util.py
@@ -2,7 +2,7 @@ from dataclasses import MISSING, dataclass
 from typing import Any, List, Optional
 
 @dataclass
-class ModelOptions:
+class ModelConfiguration:
     """base dataclass that supported fetching attributes and metas"""
 
     _name: Optional[str] = None
@@ -14,26 +14,11 @@ class ModelOptions:
     def _get_all_attributes(self) -> List[str]:
         return [k for k in self.__dataclass_fields__.keys()]
 
-    def _get_meta(
-        self, attribute_name: str, meta: str, default: Optional[Any] = None
-    ) -> Any:
-        return self.__dataclass_fields__[attribute_name].metadata.get(meta, default)
-
     def _get_name(self, attribute_name: str) -> str:
         return self.__dataclass_fields__[attribute_name].name
 
     def _get_default(self, attribute_name: str) -> Any:
         if hasattr(self, attribute_name):
-            if str(getattr(self, attribute_name)).startswith("${"):
-                return str(getattr(self, attribute_name))
-            elif str(self.__dataclass_fields__[attribute_name].default).startswith(
-                "${"
-            ):
-                return str(self.__dataclass_fields__[attribute_name].default)
-            elif (
-                getattr(self, attribute_name)
-                != self.__dataclass_fields__[attribute_name].default
-            ):
                 return getattr(self, attribute_name)
 
         f = self.__dataclass_fields__[attribute_name]
@@ -43,18 +28,6 @@ class ModelOptions:
 
     def _get_type(self, attribute_name: str) -> Any:
         return self.__dataclass_fields__[attribute_name].type
-
-    def _get_help(self, attribute_name: str) -> Any:
-        return self._get_meta(attribute_name, "help")
-
-    def _get_argparse_const(self, attribute_name: str) -> Any:
-        return self._get_meta(attribute_name, "argparse_const")
-
-    def _get_argparse_alias(self, attribute_name: str) -> Any:
-        return self._get_meta(attribute_name, "argparse_alias")
-
-    def _get_choices(self, attribute_name: str) -> Any:
-        return self._get_meta(attribute_name, "choices")
 
     @classmethod
     def from_namespace(cls, args):
@@ -70,3 +43,15 @@ class ModelOptions:
                     setattr(config, k, getattr(args, k))
 
             return config
+
+@dataclass
+class TestConfiguration(ModelConfiguration):
+
+    num_layers: int = 5
+    hidden_dim: int = 256
+    name: str = "Cool Model Configuration"
+
+class TestNestedConfiguration(ModelConfiguration):
+
+    encoder_config: ModelConfiguration = TestConfiguration(num_layers=4, hidden_dim=3, name="HSG")
+    hidden_dim: int = 333
