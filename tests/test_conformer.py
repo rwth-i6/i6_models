2b02292975a84f425fba96f2aff3cc6468a5029e
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2023-05-04 18:43:54 +0200

    implement conformer convolution part

--- tests/test_conformer.py
+++ tests/test_conformer.py
@@ -1,12 +1,23 @@
-from itertools import product
-
 import torch
 from torch import nn
+from itertools import product
 
+from i6_models.parts.conformer.convolution import ConformerConvolutionV1
 from i6_models.parts.conformer.feedforward import (
     ConformerPositionwiseFeedForwardV1,
     ConformerPositionwiseFeedForwardV1Config,
 )
+
+def test_conformer_convolution():
+    def get_output_shape(batch, time, features):
+        x = torch.randn(batch, time, features)
+        conformer_conv_part = ConformerConvolutionV1(channels=features, kernel_size=31, dropout=0.1)
+        y = conformer_conv_part(x)
+        return y.shape
+
+    assert get_output_shape(1, 50, 100) == (1, 50, 100)  # test with batch size 1
+    assert get_output_shape(10, 50, 250) == (10, 50, 250)
+    assert get_output_shape(10, 1, 50) == (10, 1, 50)  # time dim 1
 
 
 def test_ConformerPositionwiseFeedForwardV1():
