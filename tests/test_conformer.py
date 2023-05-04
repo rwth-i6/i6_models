b046bf548fac16a58953c7f82342c8e71df41b95
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2023-05-04 19:08:12 +0200

    check that kernel size is odd

--- tests/test_conformer.py
+++ tests/test_conformer.py
@@ -3,12 +3,13 @@ import torch
 
 
 def test_conformer_convolution():
-    def get_output_shape(batch, time, features):
+    def get_output_shape(batch, time, features, kernel_size=31, dropout=0.1):
         x = torch.randn(batch, time, features)
-        conformer_conv_part = ConformerConvolutionV1(channels=features, kernel_size=31, dropout=0.1)
+        conformer_conv_part = ConformerConvolutionV1(channels=features, kernel_size=kernel_size, dropout=dropout)
         y = conformer_conv_part(x)
         return y.shape
 
     assert get_output_shape(1, 50, 100) == (1, 50, 100)  # test with batch size 1
     assert get_output_shape(10, 50, 250) == (10, 50, 250)
     assert get_output_shape(10, 1, 50) == (10, 1, 50)  # time dim 1
+    assert get_output_shape(10, 10, 20, dropout=0.0) == (10, 10, 20)  # dropout 0
