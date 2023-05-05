a2a59b1654a49bf8c6f30b48d774ebd233ebe88b
Judyxujj <judyxujj@yahoo.com>
2023-05-05 13:36:16 +0200

    activation configurable

--- i6_models/parts/conformer/feedforward.py
+++ i6_models/parts/conformer/feedforward.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Callable
 import torch
 from torch import nn
 
@@ -8,17 +9,18 @@ class ConformerPositionwiseFeedForward(nn.Module):
     Conformer feedforward module
     """
 
-    def __init__(self, input_dim: int, hidden_dim: int, dropout: float = 0.1):
+    def __init__(self, input_dim: int, hidden_dim: int, dropout: float = 0.1, activation: Callable[[torch.Tensor], torch.Tensor] = nn.SiLU):
         """
         :param input_dim: input dimension
         :param hidden_dim: hidden dimension (normally set to 4*input_dim as suggested by the paper)
         :param dropout: dropout probability
+        :param activation: activation function
         """
         super().__init__()
 
         self.linear_ff = nn.Linear(in_features=input_dim, out_features=hidden_dim, bias=True)
 
-        self.swish_activation = nn.SiLU()
+        self.activation = activation()
 
         self.linear_out = nn.Linear(in_features=hidden_dim, out_features=input_dim, bias=True)
 
@@ -31,7 +33,7 @@ class ConformerPositionwiseFeedForward(nn.Module):
         """
         out_tensor = self.linear_ff(tensor)  # [B,T,F]
 
-        out_tensor = self.swish_activation(out_tensor)  # [B,T,F]
+        out_tensor = self.activation(out_tensor)  # [B,T,F]
 
         out_tensor = self.linear_out(out_tensor)  # [B,T,F]
 
