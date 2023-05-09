a2dfc98510faf1107134c5d1d8b6865d8a62f8e6
Mohammad Zeineldeen <mohammadzed5@gmail.com>
2023-05-09 22:57:50 +0200

    make act configurable, typing, remove residual

--- i6_models/parts/conformer/convolution.py
+++ i6_models/parts/conformer/convolution.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+import torch
 from torch import nn
 
 
@@ -7,11 +9,12 @@ class ConformerConvolutionV1(nn.Module):
     see also: https://github.com/espnet/espnet/blob/713e784c0815ebba2053131307db5f00af5159ea/espnet/nets/pytorch_backend/conformer/convolution.py#L13
     """
 
-    def __init__(self, channels: int, kernel_size: int, dropout: float = 0.1):
+    def __init__(self, channels: int, kernel_size: int, dropout: float = 0.1, activation: nn.Module = nn.SiLU()):
         """
         :param channels: number of channels for conv layers
         :param kernel_size: kernel size of conv layers
         :param dropout: dropout probability
+        :param activation: activation function applied after batch norm
         """
         super().__init__()
 
@@ -35,23 +38,25 @@ class ConformerConvolutionV1(nn.Module):
         self.layer_norm = nn.LayerNorm(channels)
         self.batch_norm = nn.BatchNorm1d(channels)
         self.dropout = nn.Dropout(dropout)
+        self.activation = activation
 
-    def forward(self, tensor):
+    def forward(self, tensor: torch.Tensor) -> torch.Tensor:
         """
         :param torch.Tensor tensor: input tensor of shape [B,T,F]
         :return: torch.Tensor of shape [B,T,F]
         """
-        out_tensor = self.layer_norm(tensor)
+        tensor = self.layer_norm(tensor)
 
         # conv layers expect shape [B,F,T] so we have to transpose here
-        out_tensor = out_tensor.transpose(1, 2)  # [B,F,T]
+        tensor = tensor.transpose(1, 2)  # [B,F,T]
 
-        out_tensor = self.pointwise_conv1(out_tensor)  # [B,2F,T]
-        out_tensor = nn.functional.glu(out_tensor, dim=1)  # [B,F,T]
+        tensor = self.pointwise_conv1(tensor)  # [B,2F,T]
+        tensor = nn.functional.glu(tensor, dim=1)  # [B,F,T]
 
-        out_tensor = self.depthwise_conv(out_tensor)
-        out_tensor = nn.functional.silu(self.batch_norm(out_tensor), inplace=True)  # apply swish activation
+        tensor = self.depthwise_conv(tensor)
+        tensor = self.batch_norm(tensor)
+        tensor = self.activation(tensor)
 
-        out_tensor = self.pointwise_conv2(out_tensor)
+        tensor = self.pointwise_conv2(tensor)
 
-        return tensor + self.dropout(out_tensor.transpose(1, 2))
+        return self.dropout(tensor.transpose(1, 2))
