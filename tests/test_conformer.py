8deb4ca8b51903670edec51705ce0d84fa1bcb7d
pzheng <pzheng@apptek.com>
2023-05-15 18:34:12 +0200

    use ModelConfiguration, add ConformerMHSAV1test

---
+++ tests/test_conformer.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+import torch
+
+from i6_models.parts.conformer.mhsa import ConformerMHSAV1Config, ConformerMHSAV1
+
+
+def test_ConformerMHSAV1():
+    def get_output_shape(input_shape, cfg):
+
+        input = torch.randn(input_shape)
+        output = ConformerMHSAV1(cfg)(input)
+
+        return list(output.shape)
+
+    # default parameters
+    input_shape = [3, 10, 20]  # B,T,F
+    cfg = ConformerMHSAV1Config(20, 4)
+    assert get_output_shape(input_shape, cfg) == [3, 10, 20]
+
+    # all parameters
+    input_shape = [4, 15, 32]  # B,T,F
+    cfg = ConformerMHSAV1Config(
+        32, 8, key_padding_mask=torch.randint(0, 2, input_shape[:2]) > 0, att_weights_dropout=0.2, dropout=0.3
+    )
+
+    assert get_output_shape(input_shape, cfg) == [4, 15, 32]
