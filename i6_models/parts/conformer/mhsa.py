b71c2c31c3d582ff11017e6d8162178ec14795c4
pzheng <pzheng@apptek.com>
2023-05-17 12:33:24 +0200

    use torch.nn.functional.droput, value check in Configuration,more documentation on key_padding_mask

--- i6_models/parts/conformer/mhsa.py
+++ i6_models/parts/conformer/mhsa.py
@@ -17,6 +17,10 @@ class ConformerMHSAV1Config(ModelConfiguration):
     dropout: float
     """multi-headed self attention output dropout"""
 
+    def __post_init__(self) -> None:
+        super().__post_init__()
+        assert self.input_dim % self.num_att_heads == 0, "input_dim must be dividable by num_att_heads"
+
 
 class ConformerMHSAV1(torch.nn.Module):
     """
@@ -31,15 +35,20 @@ class ConformerMHSAV1(torch.nn.Module):
         self.mhsa = torch.nn.MultiheadAttention(
             cfg.input_dim, cfg.num_att_heads, dropout=cfg.att_weights_dropout, batch_first=True
         )
-        self.dropout = torch.nn.Dropout(cfg.dropout)
+        self.dropout = cfg.dropout
 
-    def forward(self, input_tensor: torch.Tensor, key_padding_mask: Optional[torch.Tensor] = None):
+    def forward(self, input_tensor: torch.Tensor, key_padding_mask: Optional[torch.Tensor] = None) -> torch.Tensor:
 
-        # layer norm, Multi-head self attention with dropout
+        """
+        Apply layer norm and multi-head self attention and dropout
+        key_padding_mask could be a binary or float mask of shape (B, T) which will be applied/added to dot product
+        """
+
         output_tensor = self.layernorm(input_tensor)  # [B,T,F]
+
         output_tensor, _ = self.mhsa(
-            output_tensor, output_tensor, output_tensor, key_padding_mask=key_padding_mask
+            output_tensor, output_tensor, output_tensor, key_padding_mask=key_padding_mask, need_weights=False
         )  # [B,T,F]
-        output_tensor = self.dropout(output_tensor)  # [B,T,F]
+        output_tensor = torch.nn.functional.dropout(output_tensor, p=self.dropout, training=self.training)  # [B,T,F]
 
         return output_tensor
