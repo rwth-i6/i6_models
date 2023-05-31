45acaa791ad30797196f50baf3b05253df611445
Christoph M. Lüscher <chris.luscher@gmx.de>
2023-05-31 11:40:17 +0200

    fix padding

--- i6_models/parts/conformer/frontend.py
+++ i6_models/parts/conformer/frontend.py
@@ -23,6 +23,7 @@ class ConformerVGGFrontendV1Config(ModelConfiguration):
         pool_2_strides: strides of second pooling layer
         activation: activation function applied after norm
     """
+
     channels: int
     conv_kernel_size: int
     pool_1_kernel_size: int
@@ -31,47 +32,69 @@ class ConformerVGGFrontendV1Config(ModelConfiguration):
     pool_2_strides: int
     activation: Union[nn.Module, Callable[[torch.Tensor], torch.Tensor]]
 
+    def check_valid(self):
+        assert self.conv_kernel_size % 2 == 1, "ConformerVGGFrontendV1 only supports odd kernel sizes"
+        assert self.pool_1_kernel_size % 2 == 1, "ConformerVGGFrontendV1 only supports odd kernel sizes"
+        assert self.pool_2_kernel_size % 2 == 1, "ConformerVGGFrontendV1 only supports odd kernel sizes"
+
+    def __post__init__(self):
+        super().__post_init__()
+        self.check_valid()
+
 
 class ConformerVGGFrontendV1(nn.Module):
+    """
+    Convolutional Front-End
+
+    Uses explicit padding for ONNX exportability, see:
+    https://github.com/pytorch/pytorch/issues/68880
+    """
+
     def __init__(self, model_cfg: ConformerVGGFrontendV1Config):
         """
         :param model_cfg: model configuration for this module
         """
         super().__init__()
 
+        model_cfg.check_valid()
+
+        conv_padding = (model_cfg.conv_kernel_size - 1) // 2
+        pool_1_padding = (model_cfg.pool_1_kernel_size - 1) // 2
+        pool_2_padding = (model_cfg.pool_2_kernel_size - 1) // 2
+
         self.conv1 = nn.Conv2d(
             in_channels=model_cfg.channels,
             out_channels=model_cfg.channels,
             kernel_size=model_cfg.conv_kernel_size,
-            padding="same",
+            padding=conv_padding,
         )
         self.conv2 = nn.Conv2d(
             in_channels=model_cfg.channels,
             out_channels=model_cfg.channels,
             kernel_size=model_cfg.conv_kernel_size,
-            padding="same",
+            padding=conv_padding,
         )
         self.pooling1 = nn.MaxPool2d(
             kernel_size=model_cfg.pool_1_kernel_size,
             stride=model_cfg.pool_1_strides,
-            padding=1,
+            padding=pool_1_padding,
         )
         self.conv3 = nn.Conv2d(
             in_channels=model_cfg.channels,
             out_channels=model_cfg.channels,
             kernel_size=model_cfg.conv_kernel_size,
-            padding="same",
+            padding=conv_padding,
         )
         self.conv4 = nn.Conv2d(
             in_channels=model_cfg.channels,
             out_channels=model_cfg.channels,
             kernel_size=model_cfg.conv_kernel_size,
-            padding="same",
+            padding=conv_padding,
         )
         self.pooling2 = nn.MaxPool2d(
             kernel_size=model_cfg.pool_2_kernel_size,
             stride=model_cfg.pool_2_strides,
-            padding=1,
+            padding=pool_2_padding,
         )
         self.activation = model_cfg.activation
 
@@ -112,48 +135,69 @@ class ConformerVGGFrontendV2Config(ModelConfiguration):
         pool_strides: strides of first pooling layer
         activation: activation function applied after norm
     """
+
     channels: int
     conv_kernel_size: int
     pool_kernel_size: int
     pool_strides: int
     activation: Union[nn.Module, Callable[[torch.Tensor], torch.Tensor]]
 
+    def check_valid(self):
+        assert self.conv_kernel_size % 2 == 1, "ConformerVGGFrontendV2 only supports odd kernel sizes"
+        assert self.pool_kernel_size % 2 == 1, "ConformerVGGFrontendV2 only supports odd kernel sizes"
+
+    def __post__init__(self):
+        super().__post_init__()
+        self.check_valid()
+
 
 class ConformerVGGFrontendV2(nn.Module):
+    """
+    Convolutional Front-End
+
+    Uses explicit padding for ONNX exportability, see:
+    https://github.com/pytorch/pytorch/issues/68880
+    """
+
     def __init__(self, model_cfg: ConformerVGGFrontendV2Config):
         """
         :param model_cfg: model configuration for this module
         """
         super().__init__()
 
+        model_cfg.check_valid()
+
+        conv_padding = (model_cfg.conv_kernel_size - 1) // 2
+        pool_padding = (model_cfg.pool_kernel_size - 1) // 2
+
         self.conv1 = nn.Conv2d(
             in_channels=model_cfg.channels,
             out_channels=model_cfg.channels,
             kernel_size=model_cfg.conv_kernel_size,
-            padding="same",
+            padding=conv_padding,
         )
         self.conv2 = nn.Conv2d(
             in_channels=model_cfg.channels,
             out_channels=model_cfg.channels,
             kernel_size=model_cfg.conv_kernel_size,
-            padding="same",
+            padding=conv_padding,
         )
         self.pooling = nn.MaxPool2d(
             kernel_size=model_cfg.pool_kernel_size,
             stride=model_cfg.pool_strides,
-            padding=1,
+            padding=pool_padding,
         )
         self.conv3 = nn.Conv2d(
             in_channels=model_cfg.channels,
             out_channels=model_cfg.channels,
             kernel_size=model_cfg.conv_kernel_size,
-            padding="same",
+            padding=conv_padding,
         )
         self.conv4 = nn.Conv2d(
             in_channels=model_cfg.channels,
             out_channels=model_cfg.channels,
             kernel_size=model_cfg.conv_kernel_size,
-            padding="same",
+            padding=conv_padding,
         )
         self.activation = model_cfg.activation
 
