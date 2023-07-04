4fed0b9fc52287ed377bf9e11323526da3568918
Christoph M. Lüscher <chris.luscher@gmx.de>
2023-07-04 09:56:40 +0200

    add optional flag

--- i6_models/parts/frontend/protocol.py
+++ i6_models/parts/frontend/protocol.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
-from typing import Protocol, Tuple
+from typing import Optional, Protocol, Tuple
 
 
 class BaseFrontendInterfaceV1(Protocol):
-    def forward(self, tensor: torch.Tensor, sequence_mask: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    def forward(self, tensor: torch.Tensor, sequence_mask: Optional[torch.Tensor]) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
         """
         :param tensor: input tensor of shape [B,T,F]
         :param sequence_mask: masking tensor of shape [B,T], bool tensor, True == alive, False == dead
@@ -14,7 +14,7 @@ class BaseFrontendInterfaceV1(Protocol):
 
 
 class FrontendInterfaceV1(BaseFrontendInterfaceV1, nn.Module):
-    def forward(self, tensor: torch.Tensor, sequence_mask: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    def forward(self, tensor: torch.Tensor, sequence_mask: Optional[torch.Tensor]) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
         """
         :param tensor: input tensor of shape [B,T,F]
         :param sequence_mask: masking tensor of shape [B,T], bool tensor, True == alive, False == dead
