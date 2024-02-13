fix window broadcasting

i6_models/primitives/feature_extraction.py
@@ -193,7 +193,7 @@ class RasrCompatibleLogMelFeatureExtractionV1(nn.Module):
         )
 
         windowed = padded.unfold(1, size=self.win_length, step=self.hop_length)  # [B, T', W=win_length]
-        smoothed = windowed * self.window.unsqueeze(0)  # [B, T', W]
+        smoothed = windowed * self.window[None, None, :]  # [B, T', W]
 
         # compute amplitude spectrum using torch.fft.rfftn
         amplitude_spectrum = torch.abs(torch.fft.rfftn(smoothed, s=self.n_fft))  # [B, T', F=n_fft//2+1]