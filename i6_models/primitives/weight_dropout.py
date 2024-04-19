331028490574852d16566598050171ded540c465
Jingjing Xu <jxu@ccx-00.apptek.local>
2024-04-19 10:18:08 -0400

    add general weight-dropout class

--- i6_models/primitives/weight_dropout.py
+++ i6_models/primitives/weight_dropout.py
@@ -1,8 +1,10 @@
-import torch.nn as nn
-import collections
+import torch
+from torch.nn import Parameter
+
+from typing import List
 
 
-def _weight_drop(module, weights, dropout):
+def _weight_drop(module: torch.nn.module, weights: List[str], dropout: float = 0.0):
     for name_w in weights:
         w = getattr(module, name_w)
         del module._parameters[name_w]
@@ -19,11 +21,26 @@ def _weight_drop(module, weights, dropout):
     setattr(module, "forward", forward)
 
 
+class WeightDrop(torch.nn.Module):
+    """
+    Apply dropout on weights of a given nn.module
+    weight dropout paper c.f. https://ieeexplore.ieee.org/document/9468799
+    implementation c.f. https://pytorchnlp.readthedocs.io/en/latest/_modules/torchnlp/nn/weight_drop.html
+    """
+
+    def __init__(self, module: torch.nn.module, weights: List[str], dropout: float = 0.0):
+        """
+        Attributes:
+            module:
+        """
+        super(WeightDrop, self).__init__()
+        _weight_drop(module, weights, dropout)
+        self.forward = module.forward
+
+
 class WeightDropLinear(torch.nn.Linear):
     """
     Wrapper around :class:`torch.nn.Linear` that apply dropout on the weights
-    weight dropout paper c.f. https://ieeexplore.ieee.org/document/9468799
-    code c.f. https://pytorchnlp.readthedocs.io/en/latest/_modules/torchnlp/nn/weight_drop.html
     """
 
     def __init__(self, *args, weight_dropout: float = 0.0, **kwargs):
