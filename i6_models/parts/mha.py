d928b2e0654a4c3dde854c818781c820ab08d6f3
Kaloyan Nikolov <konik98@gmail.com>
2024-06-03 08:10:46 +0200

    Fixed norm to match torch's implementation

--- i6_models/parts/mha.py
+++ i6_models/parts/mha.py
@@ -3,7 +3,6 @@ __all__ = ["MultiheadAttentionV1", "MultiheadAttentionV1Config", "MHARef"]
 import math
 from dataclasses import dataclass
 import torch
-from torch import nn
 
 from i6_models.config import ModelConfiguration
 from i6_models.util import compat
@@ -22,7 +21,7 @@ class MultiheadAttentionV1Config(ModelConfiguration):
         assert self.input_dim % self.num_att_heads == 0, "input_dim must be divisible by num_att_heads"
 
 
-class MultiheadAttentionV1(nn.Module):
+class MultiheadAttentionV1(torch.nn.Module):
     def __init__(self, cfg: MultiheadAttentionV1Config):
         super().__init__()
         self.cfg = cfg
@@ -31,12 +30,12 @@ class MultiheadAttentionV1(nn.Module):
         self.dim_heads = self.input_dim // self.num_att_heads
         self.layernorm = torch.nn.LayerNorm(cfg.input_dim)
 
-        self.out_proj = nn.Linear(in_features=cfg.input_dim, out_features=cfg.input_dim, bias=True)
-        self.in_proj = nn.Linear(in_features=cfg.input_dim, out_features=3 * cfg.input_dim, bias=True)
+        self.out_proj = torch.nn.Linear(in_features=cfg.input_dim, out_features=cfg.input_dim, bias=True)
+        self.in_proj = torch.nn.Linear(in_features=cfg.input_dim, out_features=3 * cfg.input_dim, bias=True)
 
-        self.norm = math.sqrt(self.input_dim)
-        self.softmax = nn.Softmax(-1)
-        self.dropout = nn.Dropout(cfg.att_weights_dropout)
+        self.norm = math.sqrt(float(self.input_dim/self.num_att_heads))
+        self.softmax = torch.nn.Softmax(-1)
+        self.dropout = torch.nn.Dropout(cfg.att_weights_dropout)
 
 
     def forward(
@@ -84,58 +83,3 @@ class MultiheadAttentionV1(nn.Module):
         att_out = self.out_proj(att_out)
 
         return att_out, alpha
-
-
-class MHARef(nn.Module):
-    def __init__(self, d_in, d_out, num_heads, dropout=0.0, qkv_bias=False):
-        super().__init__()
-
-        assert d_out % num_heads == 0, "embed_dim is indivisible by num_heads"
-
-        self.num_heads = num_heads
-        self.head_dim = d_out // num_heads
-
-        self.qkv = torch.nn.Linear(d_in, 3 * d_out, bias=qkv_bias)
-        self.proj = torch.nn.Linear(d_in, d_out)
-        self.dropout = torch.nn.Dropout(dropout)
-
-        self.register_buffer(
-            "mask", torch.triu(torch.ones(d_in, d_in), diagonal=1)
-        )
-
-    def forward(self, x, y, z, key_padding_mask):
-        batch_size, num_tokens, embed_dim = x.shape
-
-        # (b, num_tokens, embed_dim) --> (b, num_tokens, 3 * embed_dim)
-        #print(f'ref pre :{x[0]}')
-        qkv = self.qkv(x)
-
-        # (b, num_tokens, 3 * embed_dim) --> (b, num_tokens, 3, num_heads, head_dim)
-        qkv = qkv.view(batch_size, num_tokens, 3, self.num_heads, self.head_dim)
-
-        # (b, num_tokens, 3, num_heads, head_dim) --> (3, b, num_heads, num_tokens, head_dim)
-        qkv = qkv.permute(2, 0, 3, 1, 4)
-
-        # (3, b, num_heads, num_tokens, head_dim) -> 3 times (b, num_head, num_tokens, head_dim)
-        queries, keys, values = qkv.unbind(0)
-
-        # (b, num_heads, num_tokens, head_dim) --> (b, num_heads, num_tokens, num_tokens)
-        attn_scores = queries @ keys.transpose(-2, -1)
-        attn_scores = attn_scores.masked_fill(
-            self.mask.bool()[:num_tokens, :num_tokens], -torch.inf
-        )
-
-        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**-0.5, dim=-1)
-        attn_weights = self.dropout(attn_weights)
-
-        # (b, num_heads, num_tokens, num_tokens) --> (b, num_heads, num_tokens, head_dim)
-        context_vec = attn_weights @ values
-
-        # (b, num_heads, num_tokens, head_dim) --> (b, num_tokens, num_heads, head_dim)
-        context_vec = context_vec.transpose(1, 2)
-
-        # (b, num_tokens, num_heads, head_dim) --> (b, num_tokens, embed_dim)
-        context_vec = context_vec.contiguous().view(batch_size, num_tokens, embed_dim)
-        context_vec = self.proj(context_vec)
-
-        return context_vec, attn_weights
