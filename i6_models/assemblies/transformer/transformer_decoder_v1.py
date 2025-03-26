066e9735cd558a013f0f8ff21c353f0c0823a494
Moritz Gunz <moritz.gunz@gmail.com>
2025-03-26 13:01:23 -0400

    initial commit

---
+++ i6_models/assemblies/transformer/transformer_decoder_v1.py
@@ -0,0 +1,207 @@
+from __future__ import annotations
+
+__all__ = [
+    "TransformerDecoderBlockV1Config",
+    "TransformerDecoderBlockV1",
+    "TransformerDecoderV1Config",
+    "TransformerDecoderV1",
+]
+
+import torch
+from torch import nn, Tensor
+
+from dataclasses import dataclass, field
+from typing import List, Optional, Tuple, TypedDict, Union
+
+from i6_models.config import ModelConfiguration
+from i6_models.parts.conformer import (
+    ConformerMHSARelPosV1,
+    ConformerPositionwiseFeedForwardV2,
+    ConformerPositionwiseFeedForwardV2Config,
+)
+from i6_models.parts.transformer import (
+    CausalSelfAttentionV1,
+    CausalSelfAttentionV1Config,
+    CausalSelfAttentionV1State,
+    CrossAttentionV1,
+    CrossAttentionV1Config,
+    CrossAttentionV1State,
+    DummyState,
+    ModuleWithState,
+)
+
+
+@dataclass
+class TransformerDecoderBlockV1Config(ModelConfiguration):
+    """
+    Attributes:
+        ff_cfg: Configuration for ConformerPositionwiseFeedForwardV1
+        mhsa_cfg: Configuration for CausalSelfAttentionV1
+        cross_cfg: Configuration for CrossAttentionV1
+        modules: List of modules to use for ConformerBlockV2,
+            "ff" for feed forward module, "mhsa" for multi-head self attention module, "conv" for conv module
+        scales: List of scales to apply to the module outputs before the residual connection
+    """
+
+    ff_cfg: ConformerPositionwiseFeedForwardV2Config
+    mhsa_cfg: CausalSelfAttentionV1Config
+    cross_cfg: CrossAttentionV1Config
+    modules: List[str] = field(default_factory=lambda: ["mhcsa", "cross", "ff"])
+    scales: List[float] = field(default_factory=lambda: [1.0, 1.0, 1.0])
+
+    def __post__init__(self):
+        super().__post_init__()
+
+        assert len(self.modules) == len(self.scales), "modules and scales must have same length"
+        assert all(name in ["ff", "mhcsa", "cross"] for name in self.modules), "module type not supported"
+
+
+class TransformerDecoderBlockV1State(TypedDict):
+    module_states: List[Union[CrossAttentionV1State, CausalSelfAttentionV1State]]
+
+
+class TransformerDecoderBlockV1(nn.Module, ModuleWithState[TransformerDecoderBlockV1State]):
+    def __init__(self, cfg: TransformerDecoderBlockV1Config):
+        super().__init__()
+
+        modules = []
+        for module_name in cfg.modules:
+            if module_name == "ff":
+                modules.append(DummyState(ConformerPositionwiseFeedForwardV2(cfg.ff_cfg)))
+            elif module_name == "mhcsa":
+                modules.append(CausalSelfAttentionV1(cfg.mhsa_cfg))
+            elif module_name == "cross":
+                modules.append(CrossAttentionV1(cfg.cross_cfg))
+            else:
+                raise NotImplementedError
+
+        self.module_list = nn.ModuleList(modules)
+        self.scales = cfg.scales
+
+    def get_initial_state(self) -> TransformerDecoderBlockV1State:
+        return {"module_states": [module.get_initial_state() for module in self.module_list]}
+
+    def transform_encoder_output(
+        self,
+        encoder_output: Tensor,
+        encoder_output_mask: Tensor,
+        state: TransformerDecoderBlockV1State,
+    ) -> TransformerDecoderBlockV1State:
+        new_module_state = [
+            module.transform_encoder_output(encoder_output, encoder_output_mask, module_state)
+            for module, module_state in zip(self.module_list, state["module_states"])
+        ]
+        return {**state, "module_states": new_module_state}
+
+    def forward(
+        self, labels: Tensor, labels_lens: Tensor, state: TransformerDecoderBlockV1State
+    ) -> Tuple[Tensor, TransformerDecoderBlockV1State]:
+        new_states = []
+        for module, scale, module_state in zip(self.module_list, self.scales, state["module_states"]):
+            module_output, new_mod_state = module(labels, labels_lens, module_state)
+            labels = labels + module_output * scale
+            new_states.append(new_mod_state)
+        return labels, {**state, "module_states": new_states}
+
+
+@dataclass
+class TransformerDecoderV1Config(ModelConfiguration):
+    """
+    Attributes:
+        block_cfg: Configuration for TransformerDecoderV1
+        num_layers: Number of conformer layers in the decoder
+    """
+
+    block_cfg: TransformerDecoderBlockV1Config
+    input_dropout: float
+    input_embedding_scale: Optional[float]
+    num_blocks: int
+    num_output: int
+
+
+class TransformerDecoderV1State(TypedDict):
+    block_state: List[TransformerDecoderBlockV1State]
+    pos: Tensor
+
+
+class TransformerDecoderV1(nn.Module, ModuleWithState[TransformerDecoderV1State]):
+    def __init__(self, cfg: TransformerDecoderV1Config):
+        """
+        :param cfg: configuration with subunits for transformer blocks
+        """
+        super().__init__()
+
+        self.model_dim = cfg.block_cfg.ff_cfg.input_dim
+
+        self.input_dropout = nn.Dropout(cfg.input_dropout)
+        self.input_embedding = nn.Embedding(cfg.num_output, cfg.block_cfg.ff_cfg.input_dim)
+        self.input_embedding_scale = (
+            cfg.input_embedding_scale if cfg.input_embedding_scale is not None else cfg.block_cfg.ff_cfg.input_dim**0.5
+        )
+        self.module_list = torch.nn.ModuleList(
+            [TransformerDecoderBlockV1(cfg.block_cfg) for _ in range(cfg.num_blocks)]
+        )
+        self.out_norm = nn.LayerNorm(cfg.block_cfg.ff_cfg.input_dim)
+        self.out_logits = nn.Linear(cfg.block_cfg.ff_cfg.input_dim, cfg.num_output)
+
+    def get_initial_state(self) -> TransformerDecoderV1State:
+        """:return: initial decoder state"""
+        return {
+            "block_state": [block.get_initial_state() for block in self.module_list],
+            "pos": 0,
+        }
+
+    def transform_encoder_output(
+        self,
+        encoder_output: Tensor,
+        encoder_output_mask: Tensor,
+        state: TransformerDecoderV1State,
+    ) -> TransformerDecoderV1State:
+        """
+        Process the given encoder output into input for the decoding process.
+
+        :param encoder_output: encoder output tensor, (B, T, F)
+        :param encoder_output_mask: boolean mask of valid encoder output positions,
+            `True` inside valid positions, `False` outside.
+        :param state: initial decoder state obtained by calling `get_initial_state()`.
+        """
+        new_block_state = [
+            block.transform_encoder_output(encoder_output, encoder_output_mask, block_state)
+            for block, block_state in zip(self.module_list, state["block_state"])
+        ]
+        return {**state, "block_state": new_block_state}
+
+    def forward(
+        self, labels: Tensor, labels_lens: Tensor, state: TransformerDecoderV1State
+    ) -> Tuple[Tensor, TransformerDecoderV1State]:
+        """
+        Forwards the decoder.
+
+        :param labels: existing history, shape (B..., T)
+        :param labels_lens: lengths of the labels in labels, shape (B...,)
+        :param state: decoder state obtained by running `transform_encoder_output(enc_out, enc_out_mask, s)`, where:
+            - `enc_out, enc_out_mask = forward_some_encoder(...)` and
+            - `s = get_initial_state()`.
+        """
+        x = self.input_embedding(labels) * self.input_embedding_scale
+        assert state["pos"] == 0, "not implemented for pos > 0"
+        sinus_pe = ConformerMHSARelPosV1._sinusoidal_pe(
+            torch.arange(labels.shape[-1], device=labels.device), self.model_dim
+        )
+        x = x + sinus_pe.unsqueeze(0)
+        x = self.input_dropout(x)
+
+        output = x
+        new_block_states = []
+        for block, block_state in zip(self.module_list, state["block_state"]):
+            output, new_block_state = block(output, labels_lens, block_state)
+            new_block_states.append(new_block_state)
+        new_state: TransformerDecoderV1State = {
+            **state,
+            "block_state": new_block_states,
+            "pos": state["pos"] + labels_lens,
+        }
+
+        output = self.out_norm(output)
+        output_logits = self.out_logits(output)
+        return output_logits, new_state
---
+++ i6_models/parts/transformer/cross_att.py
@@ -0,0 +1,116 @@
+__all__ = [
+    "CrossAttentionV1Config",
+    "CrossAttentionV1State",
+    "CrossAttentionV1",
+]
+
+from dataclasses import dataclass
+from typing import Literal, Optional, Tuple, TypedDict
+
+import torch
+from torch import nn, Tensor
+import torch.nn.functional as F
+
+from i6_models.config import ModelConfiguration
+from i6_models.parts.dropout import BroadcastDropout
+from i6_models.parts.transformer.util import ModuleWithState
+
+
+@dataclass
+class CrossAttentionV1Config(ModelConfiguration):
+    att_dropout: float
+    att_dropout_broadcast_axes: Optional[Literal["B", "T", "BT"]]
+    dropout: float
+    dropout_broadcast_axes: Optional[Literal["B", "T", "BT"]]
+    encoder_dim: int
+    model_dim: int
+    key_dim_total: int
+    value_dim_total: int
+    num_heads: int
+    with_bias: bool
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        assert self.att_dropout >= 0
+        assert self.att_dropout_broadcast_axes is None, "torch.sdpa does not support dropout broadcast customization"
+        assert self.dropout >= 0
+        assert self.encoder_dim > 0
+        assert self.model_dim > 0
+        assert self.num_heads > 0
+        assert self.encoder_dim % self.num_heads == 0
+        assert self.model_dim % self.num_heads == 0
+        assert self.key_dim_total % self.num_heads == 0
+        assert self.value_dim_total % self.num_heads == 0
+
+
+class CrossAttentionV1State(TypedDict):
+    k: Tensor
+    """pre-computed key for the entire encoder output"""
+    v: Tensor
+    """pre-computed value for the entire encoder output"""
+    mask: Tensor
+    """attention mask to remove padding"""
+
+
+class CrossAttentionV1(nn.Module, ModuleWithState[CrossAttentionV1State]):
+    def __init__(self, cfg: CrossAttentionV1Config):
+        super().__init__()
+
+        self.att_dropout = cfg.att_dropout
+        self.key_dim_total = cfg.key_dim_total
+        self.value_dim_total = cfg.value_dim_total
+        self.num_heads = cfg.num_heads
+
+        self.norm = nn.LayerNorm(cfg.model_dim)
+        self.q = nn.Linear(cfg.model_dim, cfg.key_dim_total, bias=cfg.with_bias)
+        self.kv = nn.Linear(cfg.encoder_dim, cfg.key_dim_total + cfg.value_dim_total, bias=cfg.with_bias)
+        self.out_proj = nn.Linear(cfg.value_dim_total, cfg.model_dim, bias=cfg.with_bias)
+        self.dropout = BroadcastDropout(cfg.dropout, cfg.dropout_broadcast_axes)
+
+    def get_initial_state(self) -> CrossAttentionV1State:
+        return {
+            "k": torch.zeros((0,)),
+            "v": torch.zeros((0,)),
+            "mask": torch.zeros((0,)),
+        }
+
+    def transform_encoder_output(
+        self,
+        encoder_output: Tensor,
+        encoder_output_mask: Tensor,
+        state: CrossAttentionV1State,
+    ) -> CrossAttentionV1State:
+        kv = self.kv(encoder_output)  # B... T 2E
+        k, v = torch.tensor_split(kv, (self.key_dim_total,), dim=-1)  # B... T E
+
+        mask = torch.where(encoder_output_mask, 0.0, float("-inf")).unsqueeze(-2).unsqueeze(-2)  # B... 1 1 T
+
+        k = torch.unflatten(k, -1, (self.num_heads, -1)).transpose(-3, -2)  # B... H L E
+        v = torch.unflatten(v, -1, (self.num_heads, -1)).transpose(-3, -2)  # B... H L E
+
+        return {**state, "k": k, "v": v, "mask": mask}
+
+    def forward(self, x: Tensor, x_lens: Tensor, state: CrossAttentionV1State) -> Tuple[Tensor, CrossAttentionV1State]:
+        """
+        Apply cross attention.
+
+        :param x: input of shape (B..., T, F)
+        """
+
+        x = self.norm(x)
+        q = self.q(x)  # B... T Ev
+        q = torch.unflatten(q, -1, (self.num_heads, -1)).transpose(-3, -2)  # B... H L Ev
+
+        att_out = F.scaled_dot_product_attention(
+            q,
+            key=state["k"],
+            value=state["v"],
+            attn_mask=state["mask"],
+            dropout_p=self.att_dropout if self.training else 0.0,
+        )  # B... H L E
+        out = att_out.transpose(-3, -2).flatten(-2)  # B... L E
+        out = self.out_proj(out)  # B... L F
+        out = self.dropout(out)
+
+        return out, state
