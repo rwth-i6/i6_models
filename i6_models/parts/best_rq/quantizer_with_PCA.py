72c9d0264d031211e8f80a515221d5ac6e49ecb2
Judyxujj <judyxujj@yahoo.com>
2025-09-29 13:36:18 +0200

    update

--- i6_models/parts/best_rq/quantizer_with_PCA.py
+++ i6_models/parts/best_rq/quantizer_with_PCA.py
@@ -34,34 +34,30 @@ class IncrementalPCAonGPU:
         self.batch_size = batch_size
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        # Set n_components_ based on n_components if provided
-        if n_components:
-            self.n_components_ = n_components
-
         # Initialize attributes to avoid errors during the first call to partial_fit
-        self.mean_ = None  # Will be initialized properly in partial_fit based on data dimensions
-        self.var_ = None  # Will be initialized properly in partial_fit based on data dimensions
-        self.n_samples_seen_ = 0
+        self.mean = None  # Will be initialized properly in partial_fit based on data dimensions
+        self.var = None  # Will be initialized properly in partial_fit based on data dimensions
+        self.n_samples_seen = 0
 
-    def _validate_data(self, X, dtype=torch.float32, copy=True):
+    def _validate_data(self, x, dtype=torch.float32, copy=True):
         """
-        Validates and converts the input data `X` to the appropriate tensor format.
+        Validates and converts the input data `x` to the appropriate tensor format.
 
         This method ensures that the input data is in the form of a PyTorch tensor and resides on the correct device (CPU or GPU).
         It also provides an option to create a copy of the tensor, which is useful when the input data should not be overwritten.
         """
-        if not isinstance(X, torch.Tensor):
-            X = torch.tensor(X, dtype=dtype).to(self.device)
-        if X.device == torch.device("cpu"):
-            X = X.to(self.device)
+        if not isinstance(x, torch.Tensor):
+            x = torch.tensor(x, dtype=dtype).to(self.device)
+        if x.device == torch.device("cpu"):
+            x = x.to(self.device)
         if copy:
-            X = X.clone()
-        return X
+            x = x.clone()
+        return x
 
     @staticmethod
-    def _incremental_mean_and_var(X, last_mean, last_variance, last_sample_count):
+    def _incremental_mean_and_var(x, last_mean, last_variance, last_sample_count):
         """
-        Computes the incremental mean and variance for the data `X`.
+        Computes the incremental mean and variance for the data `x`.
 
         :param X: The batch input data tensor with shape (n_samples, n_features).
         :param last_mean: The previous mean tensor with shape (n_features,).
@@ -69,18 +65,18 @@ class IncrementalPCAonGPU:
         :param last_sample_count: The count tensor of samples processed before the current batch.
         :return: Tuple[torch.Tensor, torch.Tensor, int]: Updated mean, variance tensors, and total sample count.
         """
-        if X.shape[0] == 0:
+        if x.shape[0] == 0:
             return last_mean, last_variance, last_sample_count
 
         # If last_mean or last_variance is None, initialize them with zeros
         if last_mean is None:
-            last_mean = torch.zeros(X.shape[1], device=X.device)
+            last_mean = torch.zeros(x.shape[1], device=x.device)
         if last_variance is None:
-            last_variance = torch.zeros(X.shape[1], device=X.device)
+            last_variance = torch.zeros(x.shape[1], device=x.device)
 
-        new_sample_count = X.shape[0]
-        new_mean = torch.mean(X, dim=0)
-        new_sum_square = torch.sum((X - new_mean) ** 2, dim=0)
+        new_sample_count = x.shape[0]
+        new_mean = torch.mean(x, dim=0)
+        new_sum_square = torch.sum((x - new_mean) ** 2, dim=0)
 
         updated_sample_count = last_sample_count + new_sample_count
 
@@ -114,7 +110,7 @@ class IncrementalPCAonGPU:
         v *= signs[:, None]
         return u, v
 
-    def fit(self, X, check_input=True):
+    def fit(self, x, check_input=True):
         """
         Fits the model with data `X` using minibatches of size `batch_size`.
 
@@ -122,8 +118,8 @@ class IncrementalPCAonGPU:
         :returnn: IncrementalPCAGPU: The fitted IPCA model.
         """
         if check_input:
-            X = self._validate_data(X)
-        n_samples, n_features = X.shape
+            x = self._validate_data(x)
+        n_samples, n_features = x.shape
         if self.batch_size is None:
             self.batch_size_ = 5 * n_features
         else:
@@ -131,12 +127,12 @@ class IncrementalPCAonGPU:
 
         for start in range(0, n_samples, self.batch_size_):
             end = min(start + self.batch_size_, n_samples)
-            X_batch = X[start:end]
-            self.partial_fit(X_batch, check_input=False)
+            x_batch = x[start:end]
+            self.partial_fit(x_batch, check_input=False)
 
         return self
 
-    def partial_fit(self, X, check_input=True):
+    def partial_fit(self, x, check_input=True):
         """
         Incrementally fits the model with batch data `X`.
 
@@ -147,52 +143,52 @@ class IncrementalPCAonGPU:
         first_pass = not hasattr(self, "components_")
 
         if check_input:
-            X = self._validate_data(X)
-        n_samples, n_features = X.shape
+            x = self._validate_data(x)
+        n_samples, n_features = x.shape
 
         if first_pass:
             self.components_ = None
         if self.n_components is None:
-            self.n_components_ = min(n_samples, n_features)
+            self.n_components = min(n_samples, n_features)
 
         col_mean, col_var, n_total_samples = self._incremental_mean_and_var(
-            X, self.mean_, self.var_, torch.tensor([self.n_samples_seen_], device=X.device)
+            x, self.mean, self.var, torch.tensor([self.n_samples_seen], device=x.device)
         )
 
         # Whitening
-        if self.n_samples_seen_ == 0:
-            X -= col_mean
+        if self.n_samples_seen == 0:
+            x -= col_mean
         else:
-            col_batch_mean = torch.mean(X, dim=0)
-            X -= col_batch_mean
+            col_batch_mean = torch.mean(x, dim=0)
+            x -= col_batch_mean
             mean_correction_factor = torch.sqrt(
-                torch.tensor((self.n_samples_seen_ / n_total_samples.item()) * n_samples, device=X.device)
+                torch.tensor((self.n_samples_seen / n_total_samples.item()) * n_samples, device=x.device)
             )
-            mean_correction = mean_correction_factor * (self.mean_ - col_batch_mean)
+            mean_correction = mean_correction_factor * (self.mean - col_batch_mean)
 
             if self.singular_values_ is not None and self.components_ is not None:
-                X = torch.vstack(
+                x = torch.vstack(
                     (
                         self.singular_values_.view((-1, 1)) * self.components_,
-                        X,
+                        x,
                         mean_correction,
                     )
                 )
 
-        U, S, Vt = torch.linalg.svd(X, full_matrices=False)
+        U, S, Vt = torch.linalg.svd(x, full_matrices=False)
         U, Vt = self._svd_flip(U, Vt, u_based_decision=False)
         explained_variance = S**2 / (n_total_samples.item() - 1)
         explained_variance_ratio = S**2 / torch.sum(col_var * n_total_samples.item())
 
-        self.n_samples_seen_ = n_total_samples.item()
-        self.components_ = Vt[: self.n_components_]
-        self.singular_values_ = S[: self.n_components_]
-        self.mean_ = col_mean
-        self.var_ = col_var
-        self.explained_variance_ = explained_variance[: self.n_components_]
-        self.explained_variance_ratio_ = explained_variance_ratio[: self.n_components_]
-        if self.n_components_ not in (n_samples, n_features):
-            self.noise_variance_ = explained_variance[self.n_components_ :].mean().item()
+        self.n_samples_seen = n_total_samples.item()
+        self.components_ = Vt[: self.n_components]
+        self.singular_values_ = S[: self.n_components]
+        self.mean = col_mean
+        self.var = col_var
+        self.explained_variance_ = explained_variance[: self.n_components]
+        self.explained_variance_ratio_ = explained_variance_ratio[: self.n_components]
+        if self.n_components not in (n_samples, n_features):
+            self.noise_variance_ = explained_variance[self.n_components :].mean().item()
         else:
             self.noise_variance_ = 0.0
         return self
@@ -213,39 +209,38 @@ class PCAProjectionQuantizer(nn.Module):
         super().__init__()
 
         self.input_dim = input_dim
-        self.distance_meature = distance_meature
 
-        self.PCA = IncrementalPCAonGPU(n_components=codebook_dim)
-        self.register_buffer("PCA_components", torch.empty((codebook_dim, input_dim)))
+        self.pca = IncrementalPCAonGPU(n_components=codebook_dim)
+        self.register_buffer("pca_components", torch.empty((codebook_dim, input_dim)))
 
         # projection matrix use Xavier initialization
-        P_init = torch.empty((input_dim, codebook_dim))
-        self.register_buffer("P", nn.init.xavier_uniform_(P_init))
+        p_init = torch.empty((input_dim, codebook_dim))
+        self.register_buffer("p", nn.init.xavier_uniform_(p_init))
 
         # normalize random matrix for codebook
-        self.register_buffer("CB", F.normalize(torch.randn(codebook_num_vars, codebook_dim)))
+        self.register_buffer("cb", F.normalize(torch.randn(codebook_num_vars, codebook_dim)))
         self.use_pca_projection = use_pca_projection
 
     def forward(
         self,
         x: torch.tensor,
         sequence_mask: torch.tensor,
-        normalise_after_PCA: bool = False,
-        PCA_update_steps: int = None,
+        normalise_after_pca: bool = False,
+        pca_update_steps: int = None,
         global_train_step: int = None,
     ) -> torch.tensor:
         if global_train_step is not None and self.use_pca_projection:
-            if (PCA_update_steps is None) or (PCA_update_steps is not None and global_train_step < PCA_update_steps):
-                self.PCA.partial_fit(x[sequence_mask])
-                self.PCA_components = self.PCA.components_
+            if (pca_update_steps is None) or (pca_update_steps is not None and global_train_step < pca_update_steps):
+                self.pca.partial_fit(x[sequence_mask])
+                self.pca_components = self.pca.components_
 
-        if self.use_pca_projection and PCA_update_steps > 0:
-            x = x @ self.PCA_components.T
-            if normalise_after_PCA:
+        if self.use_pca_projection and pca_update_steps > 0:
+            x = x @ self.pca_components.T
+            if normalise_after_pca:
                 x = F.normalize(x, dim=-1)
         else:
-            x = F.normalize(x @ self.P, dim=-1)
+            x = F.normalize(x @ self.p, dim=-1)
 
-        targets = vector_norm((self.CB.unsqueeze(1) - x.unsqueeze(1)), dim=-1).argmin(dim=1)
+        targets = vector_norm((self.cb.unsqueeze(1) - x.unsqueeze(1)), dim=-1).argmin(dim=1)
 
         return targets
