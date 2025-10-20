770201879d9752ac60afdb1b52099e5f2f8a3295
Judyxujj <judyxujj@yahoo.com>
2025-10-20 14:17:24 +0200

    Update i6_models/parts/best_rq/quantizer_with_pca.py
    Co-authored-by: Albert Zeyer <albzey@gmail.com>

--- i6_models/parts/best_rq/quantizer_with_pca.py
+++ i6_models/parts/best_rq/quantizer_with_pca.py
@@ -234,8 +234,8 @@ class PCAProjectionQuantizer(nn.Module):
 
     def forward(
         self,
-        x: torch.tensor,
-        sequence_mask: torch.tensor,
+        x: torch.Tensor,
+        sequence_mask: torch.Tensor,
         pca_update_steps: Optional[int] = None,
         global_train_step: Optional[int] = None,
     ) -> torch.tensor:
